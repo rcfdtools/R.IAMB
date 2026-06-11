3093e1123d0a6929850c712fba092e64227476af
rcfdtools <57076428+rcfdtools@users.noreply.github.com>
2026-06-11 16:34:43 -0500

    Update qgis_distribucion_porcentual.py
    General update.

--- file/src/qgis_distribucion_porcentual.py
+++ file/src/qgis_distribucion_porcentual.py
@@ -2,7 +2,7 @@
 # Area percentual distribution calculator - APD
 # This script has to be run in the QGIS Python console
 # Stop editing before run the script
-# Make sure the UC layer is selected in the Layers panel
+# Make sure the layer is selected in the Layers panel
 # Tested in QGIS 3.44.6
 
 from qgis.PyQt.QtCore import QVariant
